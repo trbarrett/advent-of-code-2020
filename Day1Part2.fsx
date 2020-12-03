c564a531c9f4ef31b9e2ad54b11ea931dcee9981
Tristian Barrett <trbarrett@gmail.com>
2020-12-03 15:35:42 +0000

    Improved Day1Part2. It was overcomplicated

--- Day1Part2.fsx
+++ Day1Part2.fsx
@@ -1,19 +1,12 @@
 #load "./Helper.fsx"
 open Helper
 let numbers = readInput "day1.txt" |> Seq.map int |> Set
-
-let combinationsOf2 = numbers |> Seq.allPairs numbers
-
-let combinationMap =
-    combinationsOf2
-    |> Seq.map (fun (x, y) -> x + y)
-    |> (flip Seq.zip) combinationsOf2
-    |> Map
+let pairs = numbers |> Seq.allPairs numbers
 
 let result =
-    combinationMap
-    |> Seq.find (fun (KeyValue (k, _)) -> numbers |> Set.contains (2020 - k))
-    |> fun (KeyValue (x, (a, b))) -> a * b * (2020 - x)
+    pairs
+    |> Seq.find (fun (x, y) -> numbers |> Set.contains (2020 - x - y))
+    |> fun (x, y) -> x * y * (2020 - x - y)
 
 printfn "%d" result
 
