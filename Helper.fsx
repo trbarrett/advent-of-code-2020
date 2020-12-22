c13ec79b35cf9654c700179dfe77e471ed981691
Tristian Barrett <trbarrett@gmail.com>
2020-12-22 09:46:12 +0000

    Add additional methods to the helper

--- Helper.fsx
+++ Helper.fsx
@@ -30,10 +30,53 @@ module Int64 =
         | _ -> None
 
 module Map =
+
+    let keys map =
+        map |> Map.toSeq |> Seq.map fst
+
+    let mapValues f map =
+        map |> Map.map (fun _ v -> f v)
+
+    /// Similar to List.choose, Seq.choose.
+    /// Applies the given function to each key, value pair in the map.
+    /// Returns a map comprised of the results for each pair where the function
+    /// returns Some(x)
+    let choose f map =
+        map
+        |> Map.toSeq
+        |> Seq.choose (fun (k, v) ->
+            match f k v with
+            | None -> None
+            | Some x -> Some (k, x))
+        |> Map
+
+    /// Similar to List.choose, Seq.choose.
+    /// Applies the given function to each value in the map.
+    /// Returns a map comprised of the results for each value of the map
+    /// where the function returns Some(x)
+    let chooseValues f map =
+        map
+        |> Map.toSeq
+        |> Seq.choose (fun (k, v) ->
+            match f v with
+            | None -> None
+            | Some x -> Some (k, x))
+        |> Map
+
+    let findOrDefault key defaultValue map =
+        map
+        |> Map.tryFind key
+        |> Option.defaultValue defaultValue
+
     let extendListValue key value m =
         match m |> Map.tryFind key with
         | None -> m |> Map.add key [value]
         | Some existing -> m |> Map.add key (value::existing)
+
+    let extendListValues key values m =
+        match m |> Map.tryFind key with
+        | None -> m |> Map.add key values
+        | Some existing -> m |> Map.add key (values@existing)
 
     let ofOneToManySeq xs : Map<'a, 'b list> =
         Seq.fold (fun m (x,y) -> m |> extendListValue x y) Map.empty xs
@@ -41,3 +84,16 @@ module Map =
     let flattenOneToMany m =
         let flatten = (fun (KeyValue(x, ys)) -> ys |> Seq.map (mkTuple x))
         Seq.collect flatten m
+
+    /// Takes two maps and merges them into a single map, using the given
+    /// function to combine the values from the two maps into a single value
+    let merge f mapA mapB =
+       let allKeys = [mapA; mapB] |> Seq.collect keys |> Seq.distinct
+
+       (Map [], allKeys)
+       ||> Seq.fold (fun acc key ->
+           let aValue = mapA |> Map.tryFind key
+           let bValue = mapB |> Map.tryFind key
+           acc |> Map.add key (f aValue bValue)
+       )
+
